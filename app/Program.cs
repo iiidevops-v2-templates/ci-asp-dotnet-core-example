566e877896446ed8ede9868d36e328795cf751dd
GitLab CI <gitlab-ci@dev7.iiidevops.org>
2024-01-08 08:10:59 +0000

    chore: squash commits between v2.4.2 and v2.4.3

--- app/Program.cs
+++ app/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ASP_MVC_example
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
